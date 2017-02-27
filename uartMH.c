1e23a2cd16db49ee91c20b68e67acaf0496e3b08
mjh96 <mjh2105@gmail.com>
2017-02-27 16:50:42 +0000

    Add files via upload
    First draft of UART file

---
+++ uartMH.c
@@ -0,0 +1,30 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+void USART0_Init( unsigned int baud )
+{
+/* Set baud rate */
+	UBRRH0 = (unsigned char)(baud>>8);
+	UBRRL0 = (unsigned char)baud;
+/* Enable receiver and transmitter */
+	UCSRnB = (1<<RXENn)|(1<<TXENn);
+/* Set frame format: 8data, 2stop bit */
+	UCSRnC = (1<<USBSn)|(3<<UCSZn0);
+}
+
+void USART1_Init( unsigned int baud )
+{
+/* Set baud rate */
+	UBRRH1 = (unsigned char)(baud>>8);
+	UBRRL1 = (unsigned char)baud;
+/* Enable receiver and transmitter */
+	UCSRnB = (1<<RXENn)|(1<<TXENn);
+/* Set frame format: 8data, 2stop bit */
+	UCSRnC = (1<<USBSn)|(3<<UCSZn0);
+}
+
+int main(void)
+{
+	USART0_Init();
+	USART1_Init();
+}
